a64d62a2f7934d5a4c2acc14354638f18de25cd7
Kaue-Catolica <kaue.lindner@catolicasc.edu.br>
2024-10-11 20:12:51 -0300

    Atualização

--- app.js
+++ app.js
@@ -47,3 +47,60 @@ function showSlider(type){
         next.click();
     }, timeAutoNext)
 }
+
+
+
+// jogos 
+
+// Jogos
+const game = [
+    {
+        title: 'Minecraft Voyage Aquatic',
+        image: 'image/mine-voyage.png',
+        description: 'Construa e explore com Minecraft! Use sua criatividade e habilidades de resolução de problemas para explorar e construir mundos subaquáticos com código.',
+        link: 'https://studio.code.org/s/aquatic/lessons/1/levels/1'
+    },
+    {
+        title: 'Jogo 2',
+        image: 'image/jogo2.png',
+        description: 'Descrição do jogo 2',
+        link: 'https://link-do-jogo2.com'
+    },
+    {
+        title: 'Jogo 3',
+        image: 'image/jogo3.png',
+        description: 'Descrição do jogo 3',
+        link: 'https://link-do-jogo3.com'
+    },
+    // ...
+];
+
+const gameList = document.querySelector('.game-list');
+
+games.forEach(game => {
+    const gameCard = document.createElement('div');
+    gameCard.className = 'game-card';
+
+    const gameTitle = document.createElement('h1');
+    gameTitle.textContent = game.title;
+
+    const gameImage = document.createElement('img');
+    gameImage.src = game.image;
+    gameImage.alt = game.title;
+
+    const gameDescription = document.createElement('p');
+    gameDescription.textContent = game.description;
+
+    const gameLink = document.createElement('a');
+    gameLink.href = game.link;
+    const gameLinkText = document.createElement('span');
+    gameLinkText.textContent = 'Comecar';
+    gameLink.appendChild(gameLinkText);
+
+    gameCard.appendChild(gameTitle);
+    gameCard.appendChild(gameImage);
+    gameCard.appendChild(gameDescription);
+    gameCard.appendChild(gameLink);
+
+    gameList.appendChild(gameCard);
+});
